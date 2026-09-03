45694071e305be0aecb87ffdb9c6c3a3c6456732
Stepan <step.cuvschinov@yandex.ru>
2026-09-03 20:19:42 +0300

    develop: добавлена реализация memchr

--- src/_string.c
+++ src/_string.c
@@ -1,19 +1,43 @@
 #include "_string.h"
 
-int main() { return 0; }
+#include <string.h>
 
-void* s21_memchr(const void* str, int c, size_t n) {}
-int s21_memcmp(const void* str1, const void* str2, size_t n) {}
-void* s21_memcpy(void* dest, const void* src, size_t n) {}
-void* s21_memset(void* str, int c, size_t n) {}
-char* s21_strncat(char* dest, const char* src, size_t n) {}
-char* s21_strchr(const char* str, int c) {}
-int s21_strncmp(const char* str1, const char* str2, size_t n) {}
-char* s21_strncpy(char* dest, const char* src, size_t n) {}
-size_t s21_strcspn(const char* str1, const char* str2) {}
-char* s21_strerror(int errnum) {}
-size_t s21_strlen(const char* str) {}
-char* s21_strpbrk(const char* str1, const char* str2) {}
-char* s21_strrchr(const char* str, int c) {}
-char* s21_strstr(const char* haystack, const char* needle) {}
-char* s21_strtok(char* str, const char* delim) {}
+int main() {
+  char* str = "Hello";
+  size_t n = 3;
+  int c = 'e';
+
+  printf("%p %p", _memchr(str, c, n), memchr(str, c, n));
+
+  return 0;
+}
+
+void* _memchr(const void* str, int c, size_t n) {
+  const unsigned char* ptr = (const unsigned char*)str;
+
+  unsigned char target = (unsigned char)c;
+  void* res = NULL;
+  while (n != 0 && res == NULL) {
+    if (*ptr == target) {
+      res = (void*)ptr;
+    }
+    ptr++;
+    n--;
+  }
+
+  return res;
+}
+// int s21_memcmp(const void* str1, const void* str2, size_t n) {}
+// void* s21_memcpy(void* dest, const void* src, size_t n) {}
+// void* s21_memset(void* str, int c, size_t n) {}
+// char* s21_strncat(char* dest, const char* src, size_t n) {}
+// char* s21_strchr(const char* str, int c) {}
+// int s21_strncmp(const char* str1, const char* str2, size_t n) {}
+// char* s21_strncpy(char* dest, const char* src, size_t n) {}
+// size_t s21_strcspn(const char* str1, const char* str2) {}
+// char* s21_strerror(int errnum) {}
+// size_t s21_strlen(const char* str) {}
+// char* s21_strpbrk(const char* str1, const char* str2) {}
+// char* s21_strrchr(const char* str, int c) {}
+// char* s21_strstr(const char* haystack, const char* needle) {}
+// char* s21_strtok(char* str, const char* delim) {}
--- src/_string.h
+++ src/_string.h
@@ -1,44 +1,43 @@
 #include <stdio.h>
-
 /* Выполняет поиск первого вхождения символа c (беззнаковый тип) в первых n
  * байтах строки, на которую указывает аргумент str.*/
-void* s21_memchr(const void* str, int c, size_t n);
+void* _memchr(const void* str, int c, size_t n);
 /* Сравнивает первые n байтов str1 и str2. */
-int s21_memcmp(const void* str1, const void* str2, size_t n);
+int _memcmp(const void* str1, const void* str2, size_t n);
 /* Копирует n символов из src в dest. */
-void* s21_memcpy(void* dest, const void* src, size_t n);
+void* _memcpy(void* dest, const void* src, size_t n);
 /* Копирует символ c (беззнаковый тип) в первые n символов строки, на которую
  * указывает аргумент str. */
-void* s21_memset(void* str, int c, size_t n);
+void* _memset(void* str, int c, size_t n);
 /* Добавляет строку, на которую указывает src, в конец строки, на которую
  * указывает dest, длиной до n символов. */
-char* s21_strncat(char* dest, const char* src, size_t n);
+char* _strncat(char* dest, const char* src, size_t n);
 /* Выполняет поиск первого вхождения символа c (беззнаковый тип) в строке, на
  * которую указывает аргумент str. */
-char* s21_strchr(const char* str, int c);
+char* _strchr(const char* str, int c);
 /* Сравнивает не более первых n байтов str1 и str2. */
-int s21_strncmp(const char* str1, const char* str2, size_t n);
+int _strncmp(const char* str1, const char* str2, size_t n);
 /* Копирует до n символов из строки, на которую указывает src, в dest. */
-char* s21_strncpy(char* dest, const char* src, size_t n);
+char* _strncpy(char* dest, const char* src, size_t n);
 /* Вычисляет длину начального сегмента str1, который полностью состоит из
  * символов, не входящих в str2. */
-size_t s21_strcspn(const char* str1, const char* str2);
+size_t _strcspn(const char* str1, const char* str2);
 /* Выполняет поиск во внутреннем массиве номера ошибки errnum и возвращает
  * указатель на строку с сообщением об ошибке. Нужно объявить макросы,
  * содержащие массивы сообщений об ошибке для операционных систем mac и linux.
  * Описания ошибок есть в оригинальной библиотеке. Проверка текущей ОС
  * осуществляется с помощью директив. */
-char* s21_strerror(int errnum);
+char* _strerror(int errnum);
 /* Вычисляет длину строки str, не включая завершающий нулевой символ. */
-size_t s21_strlen(const char* str);
+size_t _strlen(const char* str);
 /* Находит первый символ в строке str1, который соответствует любому символу,
  * указанному в str2. */
-char* s21_strpbrk(const char* str1, const char* str2);
+char* _strpbrk(const char* str1, const char* str2);
 /* Выполняет поиск последнего вхождения символа c (беззнаковый тип) в строке, на
  * которую указывает аргумент str. */
-char* s21_strrchr(const char* str, int c);
+char* _strrchr(const char* str, int c);
 /* Находит первое вхождение всей строки needle (не включая завершающий нулевой
  * символ), которая появляется в строке haystack. */
-char* s21_strstr(const char* haystack, const char* needle);
+char* _strstr(const char* haystack, const char* needle);
 /* Разбивает строку str на ряд токенов, разделенных delim. */
-char* s21_strtok(char* str, const char* delim);
+char* _strtok(char* str, const char* delim);
